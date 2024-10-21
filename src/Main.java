4d1a753d598919aed98a2dd380c2f55d60fc1a51
Dimitris Iracleous <diracleous@athtech.gr>
2024-10-21 09:34:23 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+        Customer customer = new Customer();
+        customer.setAddress("Athens");
+        customer.setSurname("Irac");
+
+        Product product = new Product();
+        product.setPrice(20.00);
+        product.setName("chips");
+
+        double fpa = 0.24;
+        Payment payment = new Payment();
+        payment.setCustomer(customer);
+        payment.setAmount(product.getPrice()*(1+fpa));
+
+        System.out.println(payment.getAmount());
+
+
+    }
+}
